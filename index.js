79ced20583d422f9841f437de3187e5d84397c3d
Collins <mutaic84@gmail.com>
2022-04-08 22:45:36 +0300

    add functions to implement filters on the array

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+function findMatching(arr, string) {
+   let drivers = arr.filter(value => value.toLowerCase() === string.toLowerCase())
+   return drivers
+}
+
+
+function fuzzyMatch(arr, letters) {
+    let drivers = arr.filter(value => value.startsWith(letters));
+    return drivers;
+}
+
+function matchName(obj, name) {
+    let matchingObject = obj.filter(item => item.name === name);
+    return matchingObject;
+}
